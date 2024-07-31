55db2aa59f3b2564abe07347f334041f5ec5cab9
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-07-31 10:46:21 +0600

    add all section

--- script.js
+++ script.js
@@ -5,36 +5,35 @@ let train = document.getElementById("train");
 let desert_moon = document.getElementById("desert-moon");
 let man = document.getElementById("man");
 
-window.addEventListener("scroll",()=>{
-    let value = window.scrollY;
-    moon.style.top = value * .9 + "px";
-    text.style.top = 95 + value * -0.2 + '%';
-    train.style.left = value * 1.5 + "px";
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+  moon.style.top = value * 0.9 + "px";
+  text.style.top = 95 + value * -0.2 + "%";
+  train.style.left = value * 1.5 + "px";
 
-    desert_moon.style.top = value * .3 + "px";
-    man.style.left = value * .6 + "px";
-    
+  desert_moon.style.top = value * 0.3 + "px";
+  man.style.left = value * 0.6 + "px";
 });
 
 // progress bar
 
-let calcScrollValue = ()=>{
-    let scrollProgress = document.getElementById("progress");
-    let pos =document.documentElement.scrollTop;
-    
-    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100)/calcHeight);
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let pos = document.documentElement.scrollTop;
 
-    if(pos > 100){
-        scrollProgress.style.display = "grid";
-    }else{
-        scrollProgress.style.display = "none";
-    }
+  let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
 
-    scrollProgress.addEventListener("click",()=>{
-        document.documentElement.scrollTop = 0;
-    });
-    scrollProgress.style.background= `conic-gradient(#194eb9 ${scrollValue}%,#67ccff ${scrollValue}%)`;
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#194eb9 ${scrollValue}%,#67ccff ${scrollValue}%)`;
 };
 
 window.onscroll = calcScrollValue;
